883399c725457553e0a97928ece6b44938f29948
Linus Mutugi <linusmutugi5178@gmail.com>
2020-02-13 09:22:36 +0300

    multiply

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var divide = function(number1, number2) {
-  return number1 / number2;
+var multiply = function(number1, number2) {
+  return number1 * number2;
 };
 var number1 = prompt("Enter a number:");
 var number2 = prompt("Enter another number:");
-var result = divide(number1, number2);
+var result = multiply(number1, number2);
 alert(result);
